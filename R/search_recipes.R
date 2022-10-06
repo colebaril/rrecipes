814619dc42cdd760dcb8cf68534e52997ad6eb0b
Cole Baril <110275137+colebaril@users.noreply.github.com>
2022-10-06 17:46:26 -0500

    Add files via upload

---
+++ R/search_recipes.R
@@ -0,0 +1,23 @@
+#' @param search_recipes
+#' Searches recipe websites for recipes.
+#' @return search_recipes
+#' @import dplyr
+#' @import rvest
+#' @import data.table
+#' @import stringr
+#' @import magrittr
+#' @export
+
+search_recipes <- function(query, site){
+
+  if("allrecipes" == site){
+    search_allrecipes(query)
+  } else if ("foodnetwork" == site){
+    search_foodnetwork(query)
+  }
+
+  else stop('No correct recipe sites entered. Supported sites include "allrecipes" and "foodnetwork".')
+}
+
+
+
