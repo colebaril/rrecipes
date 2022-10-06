62301e5fdd950869f1fe8a16b93390726a8e52e7
Cole Baril <110275137+colebaril@users.noreply.github.com>
2022-10-06 14:39:33 -0500

    Update search_allrecipes.R
    Added ability to use spaces for more specific searches.

--- R/search_allrecipes.R
+++ R/search_allrecipes.R
@@ -14,20 +14,24 @@ search_allrecipes <- function(query) {
 
   url <- 'https://www.allrecipes.com/search?q=' # This recently changed on the website.
 
-  searchquery <- paste(url, query, sep = "")
+  query <- gsub(" ", "+", query) # Change spaces in search query to `+` to work in URL.
 
-  first_page <- rvest::read_html(searchquery)
+  searchquery <- paste(url, query, sep = "") # Join query and base URL together.
+
+  first_page <- rvest::read_html(searchquery) # Extract 
   url_list <- first_page %>%
     rvest::html_elements("a") %>%
     rvest::html_attr("href")
 
-  remove.list <- paste(c("/recipes/", "/gallery/", "/article/"), collapse = '|')
+  remove.list <- paste(c("/recipes/", "/gallery/", "/article/", "/search"), collapse = '|') # Specify rows to remove where these are present. 
 
-  url_list <- data.frame(url_list) %>%
+  query <- gsub("+", "-", query, fixed = TRUE) # Change `+` in query to `-` to work with recipe URL.
+
+  url_list <- data.frame(url_list) %>% # Clean and format recipe list.
     filter(grepl(query, url_list)) %>%
     filter(!grepl(remove.list, url_list)) %>%
     unique()
 
-  url_list[c(1:10), ]
+  url_list[c(1:10), ] # Limit results to top 10 recipes. 
 
 }
