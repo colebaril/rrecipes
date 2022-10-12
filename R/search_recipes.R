405f208546b8ab6ae2e5762f845d1ecaec0c3cf6
Cole Baril <110275137+colebaril@users.noreply.github.com>
2022-10-11 19:04:54 -0500

    Update search_recipes.R

--- R/search_recipes.R
+++ R/search_recipes.R
@@ -14,6 +14,8 @@ search_recipes <- function(query, site){
     search_allrecipes(query)
   } else if ("foodnetwork" == site){
     search_foodnetwork(query)
+  } else if ("pioneerwoman" == site){
+    search_pioneerwoman()
   }
 
   else stop('No correct recipe sites entered. Supported sites include "allrecipes" and "foodnetwork".')
